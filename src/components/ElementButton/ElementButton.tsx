0629bd1a31673f2c405977de85cf367bb647aba9
firminoemanuel08 <122304402+oEmanuelFirmino@users.noreply.github.com>
2023-09-29 15:58:57 -0300

    10

--- src/components/ElementButton/ElementButton.tsx
+++ src/components/ElementButton/ElementButton.tsx
@@ -1,14 +1,15 @@
 import { Dialog, DialogTitle } from "@radix-ui/react-dialog"
 
-export function ElementButton(props: any) {
-    
+export function ElementButton(props: any ) {
+   
+
     return (
 
         <Dialog>
             <div className="inline-flex items-center flex-col">
 
-                <div className="bg-[#FE6148] w-12 h-12 rounded-2xl text-white p-2 " >
-
+                <div className={props.atribbutes}>
+                        
                     <div className="text-end text-xs">
                         {props.elementNumber}
                     </div>
@@ -19,7 +20,7 @@ export function ElementButton(props: any) {
 
                 </div>
 
-                <DialogTitle className="">{props.dialogTitle}</DialogTitle>
+                <DialogTitle >{props.dialogTitle}</DialogTitle>
 
             </div>
 
--- src/components/ElementsGroups/TransitionMetalsGroup.tsx
+++ src/components/ElementsGroups/TransitionMetalsGroup.tsx
@@ -1,46 +1,47 @@
 import ElementButton from "../ElementButton"
+import { attribute } from "../../assets/attributes"
 
 export function TransitionMetalsGroups() {
     return (
-        <div className="flex gap-3 mt-10 pr-4">
-        <ElementButton dialogTitle="Escândio" elementName="Sc" elementNumber="21"/>
-        <ElementButton dialogTitle="Titânio" elementName="Ti" elementNumber="22"/>
-        <ElementButton dialogTitle="Vanádio" elementName="V" elementNumber="23"/>
-        <ElementButton dialogTitle="Cromo" elementName="Cr" elementNumber="24"/>
-        <ElementButton dialogTitle="Manganês" elementName="Mn" elementNumber="25"/>
-        <ElementButton dialogTitle="Ferro" elementName="Fe" elementNumber="26"/>
-        <ElementButton dialogTitle="Cobalto" elementName="Co" elementNumber="27"/>
-        <ElementButton dialogTitle="Níquel" elementName="Ni" elementNumber="28"/>
-        <ElementButton dialogTitle="Cobre" elementName="Cu" elementNumber="29"/>
-        <ElementButton dialogTitle="Zinco" elementName="Zn" elementNumber="30"/>
-        <ElementButton dialogTitle="Ítrio" elementName="Y" elementNumber="39"/>
-        <ElementButton dialogTitle="Zircônio" elementName="Zr" elementNumber="40"/>
-        <ElementButton dialogTitle="Nióbio" elementName="ZNb" elementNumber="41"/>
-        <ElementButton dialogTitle="Molibdênio" elementName="Mo" elementNumber="42"/>
-        <ElementButton dialogTitle="Tecnécio" elementName="Tc" elementNumber="43"/>
-        <ElementButton dialogTitle="Rutênio" elementName="Ru" elementNumber="44"/>
-        <ElementButton dialogTitle="Ródio" elementName="Rh" elementNumber="42"/>
-        <ElementButton dialogTitle="Paládio" elementName="Pd" elementNumber="46"/>
-        <ElementButton dialogTitle="Prata" elementName="Ag" elementNumber="47"/>
-        <ElementButton dialogTitle="Cádmio" elementName="Cd" elementNumber="48"/>
-        <ElementButton dialogTitle="Háfnio" elementName="Hf" elementNumber="72"/>
-        <ElementButton dialogTitle="Tântalo" elementName="Ta" elementNumber="73"/>
-        <ElementButton dialogTitle="Tungstênio" elementName="W" elementNumber="74"/>
-        <ElementButton dialogTitle="Rênio" elementName="Re" elementNumber="75"/>
-        <ElementButton dialogTitle="Ósmio" elementName="Os" elementNumber="76"/>
-        <ElementButton dialogTitle="Irídio" elementName="Ir" elementNumber="77"/>
-        <ElementButton dialogTitle="Platina" elementName="Pt" elementNumber="78"/>
-        <ElementButton dialogTitle="Ouro" elementName="Au" elementNumber="79"/>
-        <ElementButton dialogTitle="Mercúrio" elementName="Hg" elementNumber="80"/>
-        <ElementButton dialogTitle="Rutherfórdio" elementName="Rf" elementNumber="104"/>
-        <ElementButton dialogTitle="Dúbnio" elementName="Db" elementNumber="105"/>
-        <ElementButton dialogTitle="Seabórgio" elementName="Sg" elementNumber="106"/>
-        <ElementButton dialogTitle="Bóhrio" elementName="Bh" elementNumber="107"/>
-        <ElementButton dialogTitle="Hássio" elementName="Hs" elementNumber="108"/>
-        <ElementButton dialogTitle="Meitnério" elementName="Mt" elementNumber="109"/>
-        <ElementButton dialogTitle="Darmstádio" elementName="Ds" elementNumber="110"/>
-        <ElementButton dialogTitle="Roentgênio" elementName="Rg" elementNumber="111"/>
-        <ElementButton dialogTitle="Copernício" elementName="Cn" elementNumber="112"/>
+        <div className="flex gap-3 mt-10 p">
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Escândio" elementName="Sc" elementNumber="21"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Titânio" elementName="Ti" elementNumber="22"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Vanádio" elementName="V" elementNumber="23"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Cromo" elementName="Cr" elementNumber="24"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Manganês" elementName="Mn" elementNumber="25"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Ferro" elementName="Fe" elementNumber="26"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Cobalto" elementName="Co" elementNumber="27"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Níquel" elementName="Ni" elementNumber="28"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Cobre" elementName="Cu" elementNumber="29"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Zinco" elementName="Zn" elementNumber="30"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Ítrio" elementName="Y" elementNumber="39"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Zircônio" elementName="Zr" elementNumber="40"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Nióbio" elementName="ZNb" elementNumber="41"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Molibdênio" elementName="Mo" elementNumber="42"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Tecnécio" elementName="Tc" elementNumber="43"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Rutênio" elementName="Ru" elementNumber="44"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Ródio" elementName="Rh" elementNumber="42"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Paládio" elementName="Pd" elementNumber="46"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Prata" elementName="Ag" elementNumber="47"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Cádmio" elementName="Cd" elementNumber="48"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Háfnio" elementName="Hf" elementNumber="72"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Tântalo" elementName="Ta" elementNumber="73"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Tungstênio" elementName="W" elementNumber="74"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Rênio" elementName="Re" elementNumber="75"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Ósmio" elementName="Os" elementNumber="76"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Irídio" elementName="Ir" elementNumber="77"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Platina" elementName="Pt" elementNumber="78"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Ouro" elementName="Au" elementNumber="79"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Mercúrio" elementName="Hg" elementNumber="80"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Rutherfórdio" elementName="Rf" elementNumber="104"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Dúbnio" elementName="Db" elementNumber="105"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Seabórgio" elementName="Sg" elementNumber="106"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Bóhrio" elementName="Bh" elementNumber="107"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Hássio" elementName="Hs" elementNumber="108"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Meitnério" elementName="Mt" elementNumber="109"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Darmstádio" elementName="Ds" elementNumber="110"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Roentgênio" elementName="Rg" elementNumber="111"/>
+        <ElementButton atribbutes={attribute[0].className} dialogTitle="Copernício" elementName="Cn" elementNumber="112"/>
         </div>
     )
 }
