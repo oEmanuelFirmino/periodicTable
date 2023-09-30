868400cea39128ae012f269246798a27f3eaee57
FirminoEmanuell <122304402+oEmanuelFirmino@users.noreply.github.com>
2023-09-30 13:18:26 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,49 @@
-
+import { Menu } from "lucide-react";
 
 export function Header() {
+  if (Number(screen.width) <= 1000) {
     return (
-        <div className="
-        bg-black
-        w-full
-        h-24
-        text-2xl
-        font-bold
-        text-white
-        flex
-        items-center
-        p-6
-        mb-10
-        ">
-            <h1 className='title'>Tabela Periódica</h1>
-        </div>
-    )
+      <div
+        className="
+            bg-black
+            w-full
+            h-24
+            text-2xl
+            font-bold
+            text-white
+            flex
+            items-center
+            p-6
+            mb-10
+            "
+      >
+        <h1 className="title">Tabela Periódica</h1>
+      </div>
+    );
+  } else if (Number(screen.width) > 1000) {
+    return (
+      <div
+        className="
+              bg-[#1a1a1a]
+              w-full
+              h-24
+              text-6xl
+              font-bold
+              text-center
+              text-white
+              inline-flex
+              items-center
+              p-6
+              mb-10
+              border-b-2
+              border-zinc-100
+              justify-center
+              "
+      >
+        <Menu className="basis-auto" />
+
+        <h1 className="basis-auto grow">Tabela Periódica</h1>
+      </div>
+    );
+  }
 }
